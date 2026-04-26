ad9a102a5ba7e2d464cf37d9c6c3a193ce3397fe
dandrok <patryk.stusik@gmail.com>
2026-04-26 20:44:15 +0200

    chore: remove not working q button from running screen

--- src/components/RunningScreen.tsx
+++ src/components/RunningScreen.tsx
@@ -1,12 +1,12 @@
 import { Box, Text } from "ink"
-
+// TODO: add [q] to quit
 export const RunningScreen = () => {
 
   return (
     <Box flexDirection="column">
       <Box marginTop={1}>
         <Text>
-          [p] pause [r] resume [q] quit
+          [p] pause [r] resume
         </Text>
       </Box>
     </Box>

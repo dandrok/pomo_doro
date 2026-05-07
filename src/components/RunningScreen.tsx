d70e8e2a2fa1d07eb2190beb8ff78aaaf24e6a82
dandrok <patryk.stusik@gmail.com>
2026-05-07 15:36:01 +0200

    refactor: update running screen component

--- src/components/RunningScreen.tsx
+++ src/components/RunningScreen.tsx
@@ -1,10 +1,10 @@
 import { Box, Text } from "ink";
-// TODO: add [q] to quit
+// TODO: q logic still missing
 export const RunningScreen = () => {
   return (
     <Box flexDirection="column">
       <Box marginTop={1}>
-        <Text>[p] pause [r] resume</Text>
+        <Text>[p] pause [r] resume [q] quit</Text>
       </Box>
     </Box>
   );
